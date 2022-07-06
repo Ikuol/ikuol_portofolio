6bee47e532bbe46c504304d5a1e4278bea96f488
ikuol <ikuolsogbohossou@gmail.com>
2022-07-06 19:27:18 +0200

    +1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 let loaderel=document.querySelector(".loaderelement");
 
 window.addEventListener("load", function(){
-    this.setTimeout(loader,2000);
-    this.setTimeout(fadeOut,2000);
+    this.setTimeout(loader,2500);
+    this.setTimeout(fadeOut,2500);
     
 });
-
 
   function fadeOut(){
     window.location.replace("../ikuol_portofolio/contact.html");
